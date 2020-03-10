fb447423e315477de518ba54ccc4b66cbf8e5ee5
zubairdv10 <zubairdevries127@gmail.com>
2020-03-10 10:09:32 +0200

    car names

---
+++ cars.py
@@ -0,0 +1,9 @@
+#listing numbers
+myList=[23,82,42,84,33]
+print(myList)
+myList.insert(85,12)
+print(myList)
+Cars={"Aston Martin","VW","Ford","BMW","Nissan"}
+print(type(Cars))
+Cars1=("Aston Martin","VW","Ford","BMW","Nissan")
+print(type(Cars1))
